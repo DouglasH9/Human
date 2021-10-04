27e7b1aea6e117cc8fc8c1ae274dc6f146e7cf8d
Doug Herman <douglash9882@gmail.com>
2021-10-03 23:09:43 -0500

    humanNinjaSamurai

--- HumanProj.cs
+++ HumanProj.cs
@@ -1,42 +1,65 @@
+using System.Collections.Generic
+
 namespace HumanProj
 {
-        public class Human
+    public class Human
         {
-            // Fields for Human
             public string Name;
             public int Strength;
             public int Intelligence;
             public int Dexterity;
             private int health;
+             
             public int Health
             {
-                get {return health; }
+                get { return health; }
             }
-            
-            public Human(string name, int strength, int intelligence, int dexterity)
+             
+            public Human(string name)
             {
                 Name = name;
-                Strength = strength;
-                Intelligence = intelligence;
-                Dexterity = dexterity;
+                Strength = 3;
+                Intelligence = 3;
+                Dexterity = 3;
                 health = 100;
             }
-
-            public Human(string name, int str, int intel, int dex, int healthAmnt)
+             
+            public Human(string name, int str, int intel, int dex, int hp)
             {
                 Name = name;
                 Strength = str;
                 Intelligence = intel;
                 Dexterity = dex;
-                health = healthAmnt;
+                health = hp;
             }
-            
+             
+            // Build Attack method
             public int Attack(Human target)
             {
-                int dmg = Strength * 5;
+                int dmg = Strength * 3;
                 target.health -= dmg;
+                Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
                 return target.health;
             }
+        }
+
+    public class Wizard : Human
+    {
+        public Wizard(string name)
+        {
+            Name = name;
+            Intelligence = 25;
+            health = 50;
+        }
+    }
+    public class Ninja : Human
+    {
+        public Ninja(string name)
+        {
+            Name = name;
+            Dexterity = 175;
             
         }
+    }
+
 }
